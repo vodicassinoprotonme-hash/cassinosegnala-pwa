2526192196181011a54601553ef87f87af5269ea
vodicassinoprotonme-hash <vodicassinoproton.me@gmail.com>
2025-09-01 08:29:40 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+// Service Worker basico per PWA cache-first
+const CACHE = 'cassinosignal-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './service-worker.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
